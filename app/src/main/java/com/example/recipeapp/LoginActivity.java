25000dc1b82e4babde7e91de785eb8422581325e
Dhruvansh_Bhatt <121612258+Dsbhatt313@users.noreply.github.com>
2024-05-04 12:52:48 +0530

    Added comment

--- app/src/main/java/com/example/recipeapp/LoginActivity.java
+++ app/src/main/java/com/example/recipeapp/LoginActivity.java
@@ -21,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     @Override
+    //This is an OnCreate method
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
